f0a1fe0b7f54ae691c243e140a4cf70101b90792
Md Rayhan Kobir <rayhankobir236@gmail.com>
2025-03-20 22:35:07 -0700

    validation added

--- src/DynamicFormGenerator.php
+++ src/DynamicFormGenerator.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Rayhan\DynamicFormGenerator;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 class DynamicFormGenerator
 {
@@ -16,9 +18,11 @@ class DynamicFormGenerator
         $method = $this->config['method'] ?? 'POST';
         $rowClass = $this->config['row_class'] ?? 'gy-3';
 
-        $html = '<form method="' . $method . '" action="' . $this->config['action'] . '" enctype="multipart/form-data">';
+        $action = $this->getActionUrl($this->config['action']);
+        $html = '<form method="' . $method . '" action="' . $action . '" enctype="multipart/form-data">';
         $html .= csrf_field();
-
+        //hidden input
+        $html .= '<input type="hidden" name="form_config" value="' . htmlspecialchars(json_encode($this->config)) . '">';
         $html .= '<div class="row ' . $rowClass . '">';
 
         foreach ($this->config['fields'] as $field) {
@@ -51,6 +55,7 @@ class DynamicFormGenerator
         $fieldClass = $field['class'] ?? 'form--control';
         $labelClass = $field['label_class'] ?? 'form--label';
         $colClass = $field['col'] ?? 'col-md-6';
+        $required = $field['required'] ?? false;
 
         //col
         $html = '<div class="' . $colClass . '">';
@@ -58,7 +63,11 @@ class DynamicFormGenerator
 
         // label
         if (!in_array($type, ['hidden', 'submit', 'button'])) {
-            $html .= '<label class="' . $labelClass . '" for="' . $id . '">' . $label . '</label>';
+            $html .= '<label class="' . $labelClass . '" for="' . $id . '">' . $label;
+            if ($required) {
+                $html .= ' <span class="text-danger">*</span>'; 
+            }
+            $html .= '</label>';
         }
 
         // filed
@@ -109,7 +118,7 @@ class DynamicFormGenerator
         // validation error
         if ($errors = session('errors')) {
             if ($errors->has($name)) {
-                $html .= '<span style="color: red;">' . $errors->first($name) . '</span>';
+                $html .= '<span color="text-danger">' . $errors->first($name) . '</span>';
             }
         }
 
@@ -127,6 +136,96 @@ class DynamicFormGenerator
         $class = $button['class'] ?? 'btn btn-primary';
 
         return '<button type="' . $type . '" name="' . $name . '" id="' . $id . '" class="' . $class . '">' . $label . '</button>';
+    }
+
+    private function getActionUrl($action)
+    {
+        if (Route::has($action)) {
+            return route($action); 
+        }
+        return $action;
+    }
+
+    // input validation
+    public function validate($request)
+    {
+        $rules = [];
+
+        foreach ($this->config['fields'] as $field) {
+            $name = str_replace('[]', '', $field['name']);
+            $type = $field['type'] ?? 'text';
+            $required = $field['required'] ?? false;
+
+            $fieldRules = [];
+
+            // check required
+            if ($required) {
+                $fieldRules[] = 'required';
+            } else {
+                $fieldRules[] = 'nullable'; 
+            }
+
+            // rules set
+            switch ($type) {
+                case 'text':
+                case 'textarea':
+                    $fieldRules[] = 'string';
+                    $fieldRules[] = 'max:255';
+                    break;
+                case 'email':
+                    $fieldRules[] = 'email';
+                    break;
+                case 'password':
+                    if ($required) {
+                        $fieldRules[] = 'min:8';
+                    }
+                    break;
+                case 'number':
+                case 'range':
+                    $fieldRules[] = 'numeric';
+                    if (isset($field['min'])) {
+                        $fieldRules[] = 'min:' . $field['min'];
+                    }
+                    if (isset($field['max'])) {
+                        $fieldRules[] = 'max:' . $field['max'];
+                    }
+                    break;
+                case 'date':
+                    $fieldRules[] = 'date';
+                    break;
+                case 'select':
+                case 'radio':
+                    if (isset($field['options'])) {
+                        $validOptions = implode(',', array_keys($field['options']));
+                        $fieldRules[] = 'in:' . $validOptions;
+                    }
+                    break;
+                case 'checkbox':
+                    if (isset($field['options'])) {
+                        $validOptions = implode(',', array_keys($field['options']));
+                        $fieldRules[] = 'array';
+                        $fieldRules[] = 'in:' . $validOptions;
+                    }
+                    break;
+                case 'file':
+                    $fieldRules[] = 'file';
+                    $fieldRules[] = 'mimes:jpeg,png,jpg,gif';
+                    $fieldRules[] = 'max:2048'; 
+                    break;
+            }
+
+            $rules[$name] = implode('|', $fieldRules);
+        }
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        return true; 
     }
 
 }
